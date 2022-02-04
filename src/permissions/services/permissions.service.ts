56445e422c3437b27338685dc50beba2d89dc59f
gmichalopulos <gregomicha@gmail.com>
2022-02-04 06:47:19 -0300

    chore: agregar o actualizar permisos

--- src/permissions/services/permissions.service.ts
+++ src/permissions/services/permissions.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { InjectConnection } from '@nestjs/mongoose';
-import { Collection, Connection } from 'mongoose';
+import { Collection, Connection, ObjectId } from 'mongoose';
 import { PutPermissionsDto } from '../dtos/put.permission.dto';
+import { PutPermissionsFieldDto } from '../dtos/put.permission.field.dto';
 
 @Injectable()
 export class PermissionsService {
   private readonly permissions: Collection;
+  private readonly models: Collection;
 
   constructor(@InjectConnection() private connection: Connection) {
     this.permissions = this.connection.collection('permissions');
+    this.models = this.connection.collection('models');
   }
 
   async get(initiative: string) {
@@ -21,36 +24,45 @@ export class PermissionsService {
 
   async put(request: PutPermissionsDto) {
     
-    const currentPermissions = await this.getCurrentPermissionsForInitiative(request.initiative);
+    const permission = await this.getCurrentPermissionsForInitiative(request.initiative);
     
-    request.fields.forEach((field) => {
-      field.access_key.split(',')
-        .map(key => key ? `${field.property}.${key}` : field.property)
-        .forEach(prop => this.updatePropInNode(prop, currentPermissions));
-    });
+    this.updatePermissionFields(request.fields, permission.fields);
 
-    const permission = {
-      initiative: request.initiative,
-      fields: currentPermissions,
-    };
-
-    //TODO: Ver de actualizar o agregar
-    this.connection.collection('permissions').insertOne(permission);
+    this.addOrUpdatePermission(permission);
   }
 
   private async getCurrentPermissionsForInitiative(initiative: string): Promise<any> {
-    //TODO: Aca buscar si ya existe
-    return await this.connection.collection('models').findOne({}, { projection: {'_id':0}});
+    let permission = await this.permissions.findOne({initiative});
+    if(!permission) permission = await this.models.findOne({}, { projection: {'_id':0}});
+    permission.initiative = initiative;
+    return permission;
   }
 
-  private updatePropInNode(prop: string, node: any) {
+  private updatePermissionFields(requestFields: PutPermissionsFieldDto[], currentFields: any) {
+    requestFields.forEach((field) => {
+      field.access_key.split(',')
+        .map(key => key ? `${field.property}.${key}` : field.property)
+        .forEach(prop => this.addOrUpdatePropInNode(prop, currentFields));
+    });
+  }
+  
+  private addOrUpdatePropInNode(prop: string, node: any) {
     const [head, ...tail] = prop.split('.');
     if(tail.length > 0) {
-        if(!node[head]) node[head] = {};
-        this.updatePropInNode(tail.join('.'), node[head]);
+        if(!node[head] || node[head] === true) node[head] = {};
+        this.addOrUpdatePropInNode(tail.join('.'), node[head]);
     }
     else {
         node[head] = true;
+    }
+  }
+
+  private addOrUpdatePermission(permission: any) {
+    if(permission._id) {
+      this.permissions.updateOne({ _id: permission._id }, { $set: permission });
+    }
+    else {
+      this.permissions.insertOne(permission);
     }
   }
 }
